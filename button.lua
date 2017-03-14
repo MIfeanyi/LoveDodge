5da738e66ae1c2a71b5e301c7fed4611e1c45309
Michael Redford <michaeliredford@gmail.com>
2017-03-14 22:10:31 +0100

    updated to use image manager

--- button.lua
+++ button.lua
@@ -1,20 +1,22 @@
+local padding = 5
 button = class("button",{
-    pressed = false,x,y,h,w,img, maxTime = 0.20, waitTime})
+    pressed = false,x,y,h,w,img,id,text="button", maxTime = 0.20, waitTime})
 function button:init(x,y,h,w)
   self.x,self.y,self.h,self.w = x,y,h,w
-  --self.img = love.graphics.newImage(filename)
+  --self.img = love.graphics.newImage(id)
 end
 
-function button:load(x,y,filename)
+function button:load(x,y,text,id)
   print("x: ",x)
   print("y: ", y)
-  print("filename: ", filename)
+  print("id: ", id)
   print("Clicked: ", self.pressed)
   self.x = x
   self.y = y
-  self.img = love.graphics.newImage(filename)
-  self.h = self.img:getHeight()
-  self.w = self.img:getWidth()
+  self.text=text
+  self.id = id
+  self.h = getImage(id):getHeight()
+  self.w = getImage(id):getWidth()
   self.pressed = false
 end
 
@@ -38,5 +40,6 @@ function button:hover()
 end
 
 function button:draw()
-  love.graphics.draw(self.img, self.x, self.y)  
+  love.graphics.draw(getImage(self.id), self.x, self.y)  
+  love.graphics.print(self.text,self.x+padding,self.y+self.h*.3)
 end
